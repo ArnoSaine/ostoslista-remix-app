42355dfa3d030ba98b2d702efefd3b77dcc971c4
Arno Saine <arno@mowhi.com>
2022-08-24 11:32:50 +0300

    Ostoslistan lukeminen tiedostosta

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,31 +1,30 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getOstoslista } from "~/api.server";
+
+export const loader = async () => {
+  const ostoslista = await getOstoslista();
+  return json(ostoslista);
+};
+
 export default function Index() {
+  const ostoslista = useLoaderData();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Ostoslista</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {ostoslista.map((ostos, index) => {
+          const nimi = ostos.nimi;
+          if (ostos.hankittu) {
+            return (
+              <li key={index}>
+                <s>{nimi}</s>
+              </li>
+            );
+          }
+          return <li key={index}>{nimi}</li>;
+        })}
       </ul>
     </div>
   );
